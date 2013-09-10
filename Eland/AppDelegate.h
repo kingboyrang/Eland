9e839388df59e7fdb2fa8ecee38b3b3d9e12560f
kingboyrang <aJia@bago.com.tw>
2013-09-10 17:12:42 +0800

    Initial Commit

---
+++ Eland/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Eland
+//
+//  Created by aJia on 13/9/10.
+//  Copyright (c) 2013年 rang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Eland/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Eland
+//
+//  Created by aJia on 13/9/10.
+//  Copyright (c) 2013年 rang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
